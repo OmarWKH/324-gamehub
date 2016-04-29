0bb7481a77e198e8f80455472d0eff0412812472
aaimsh <aaimsh101@gmail.com>
2016-04-29 14:38:04 +0300

    Added country field

--- GameHub/forms.py
+++ GameHub/forms.py
@@ -1,16 +1,15 @@
 from django import forms
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-
+from registration.forms import RegistrationFormUniqueEmail
+from .models import UserProfile
 
 class LogInForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
 
-class RegisterationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+class UserProfileRegistrationForm(RegistrationFormUniqueEmail):
 
     class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        model = UserProfile
+        fields = ['first_name', 'last_name', 'username', 'country', 'email', 'password1', 'password2']
